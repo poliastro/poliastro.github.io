f34555aaa4d9700da3c484ff1fee774e8efccef3
Juan Luis Cano Rodríguez <juanlu001@gmail.com>
2019-05-13 13:00:12 +0200

    Fix site URL

--- publishconf.py
+++ publishconf.py
@@ -7,7 +7,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://poliastro.github.io'
+SITEURL = 'https://blog.poliastro.space'
 RELATIVE_URLS = False
 
 SHOW_FEED = True
