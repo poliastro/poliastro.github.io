b188664b9e01177b7f798f4f04c7be3dfe28b182
DhruvSondhi <dhruvsondhi05@gmail.com>
2021-05-07 14:21:31 +0530

    Changing url & removing submodule dependence

--- publishconf.py
+++ publishconf.py
@@ -7,7 +7,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://poliastro.space'
+SITEURL = 'https://www.poliastro.space'
 RELATIVE_URLS = False
 
 SHOW_FEED = True
